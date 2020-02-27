d2ee1f60b987256f20ff17621f99bcfcaa17e513
Matt Brucker <mpbrucker@gmail.com>
2020-02-27 00:20:21 -0500

    Making params for open_playback_device work.

--- src/pcm.c
+++ src/pcm.c
@@ -7,14 +7,13 @@ to the default PCM device for 5 seconds of data.
 
 /* Use the newer ALSA API */
 #define ALSA_PCM_NEW_HW_PARAMS_API
+#define TIME 5000000
 
 #include "pcm.h"
 
-void open_playback_device(snd_pcm_t **handle, snd_pcm_hw_params_t **params, snd_pcm_uframes_t * frame_size, char * dev_name) {
-    unsigned int val = 44100;
+void open_playback_device(snd_pcm_t **handle, snd_pcm_hw_params_t **params, snd_pcm_uframes_t * frame_size, unsigned int * sample_rate, char * dev_name) {
     int dir;
-    int res = snd_pcm_open(handle, dev_name,
-                        SND_PCM_STREAM_PLAYBACK, 0);
+    int res = snd_pcm_open(handle, dev_name, SND_PCM_STREAM_PLAYBACK, 0);
 
     // Allocate hardware parameters object and set defaults.
     snd_pcm_hw_params_alloca(params);
@@ -23,13 +22,13 @@ void open_playback_device(snd_pcm_t **handle, snd_pcm_hw_params_t **params, snd_
     snd_pcm_hw_params_set_format(*handle, *params, SND_PCM_FORMAT_S16_LE);
     snd_pcm_hw_params_set_channels(*handle, *params, 2);
     // Set sample rate and period size (_near sets parameter space as close as possible)
-    snd_pcm_hw_params_set_rate_near(*handle, *params, &val, &dir);
+    snd_pcm_hw_params_set_rate_near(*handle, *params, &sample_rate, &dir);
     snd_pcm_hw_params_set_period_size_near(*handle, *params, frame_size, &dir);
 
     res = snd_pcm_hw_params(*handle, *params);
     // Get period size and time
     snd_pcm_hw_params_get_period_size(*params, frame_size, &dir);
-    snd_pcm_hw_params_get_period_time(*params, &val, &dir);
+    snd_pcm_hw_params_get_period_time(*params, &sample_rate, &dir);
 
 }
 
@@ -37,13 +36,16 @@ void open_playback_device(snd_pcm_t **handle, snd_pcm_hw_params_t **params, snd_
 int main () {
     long loops;
     int size;
+    int res;
     snd_pcm_t *handle;
     snd_pcm_hw_params_t *params;
     char *buffer;
     snd_pcm_uframes_t frames = 16;
+    unsigned int sample_rate = 44100;
 
 
-    open_playback_device(&handle, &params, "default");
+
+    open_playback_device(&handle, &params, &frames, &sample_rate, "default");
 
 
     size = frames * 4; /* 2 bytes/sample, 2 channels */
@@ -51,12 +53,11 @@ int main () {
 
     /* 5 seconds in microseconds divided by
     * period time */
-    loops = 5000000 / val;
+    loops = TIME / sample_rate;
 
-    while (loops > 0) {
-    loops--;
-    rc = read(0, buffer, size);
-    rc = snd_pcm_writei(handle, buffer, frames);
+    for (; loops > 0; loops--) {
+        res = read(0, buffer, size);
+        res = snd_pcm_writei(handle, buffer, frames);
     }
 
     snd_pcm_drain(handle);
