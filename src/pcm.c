377b8e5852fb9641e828b49fd7059dd15a4d6002
Matt Brucker <mpbrucker@gmail.com>
2020-02-27 00:23:57 -0500

    Changing playback to async mode.

--- src/pcm.c
+++ src/pcm.c
@@ -13,7 +13,7 @@ to the default PCM device for 5 seconds of data.
 
 void open_playback_device(snd_pcm_t **handle, snd_pcm_hw_params_t **params, snd_pcm_uframes_t * frame_size, unsigned int * sample_rate, char * dev_name) {
     int dir;
-    int res = snd_pcm_open(handle, dev_name, SND_PCM_STREAM_PLAYBACK, 0);
+    int res = snd_pcm_open(handle, dev_name, SND_PCM_STREAM_PLAYBACK, SND_PCM_ASYNC);
 
     // Allocate hardware parameters object and set defaults.
     snd_pcm_hw_params_alloca(params);
@@ -22,13 +22,13 @@ void open_playback_device(snd_pcm_t **handle, snd_pcm_hw_params_t **params, snd_
     snd_pcm_hw_params_set_format(*handle, *params, SND_PCM_FORMAT_S16_LE);
     snd_pcm_hw_params_set_channels(*handle, *params, 2);
     // Set sample rate and period size (_near sets parameter space as close as possible)
-    snd_pcm_hw_params_set_rate_near(*handle, *params, &sample_rate, &dir);
+    snd_pcm_hw_params_set_rate_near(*handle, *params, sample_rate, &dir);
     snd_pcm_hw_params_set_period_size_near(*handle, *params, frame_size, &dir);
 
     res = snd_pcm_hw_params(*handle, *params);
     // Get period size and time
     snd_pcm_hw_params_get_period_size(*params, frame_size, &dir);
-    snd_pcm_hw_params_get_period_time(*params, &sample_rate, &dir);
+    snd_pcm_hw_params_get_period_time(*params, sample_rate, &dir);
 
 }
 
@@ -42,8 +42,6 @@ int main () {
     char *buffer;
     snd_pcm_uframes_t frames = 16;
     unsigned int sample_rate = 44100;
-
-
 
     open_playback_device(&handle, &params, &frames, &sample_rate, "default");
 
