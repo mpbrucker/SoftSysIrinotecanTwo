58ce16da17f4eb3497ec206a2b7474cafc089734
William Derksen <wderksen98@gmail.com>
2020-04-05 09:49:42 -0400

    leftover code from first pass at Input handling

--- src/keyInput.c
+++ src/keyInput.c
@@ -1,7 +1,39 @@
 #include "keyInput.h"
 
+#include <assert.h>
+#include <unistd.h>
+
+#define NIL (0)
 
 int main () {
-	int x = 0;
-	return 0;
+	//Ncurses Attempt
+	/*initscr();
+	cbreak();
+	noecho();
+	nodelay(stdscr, TRUE);
+	notimeout(stdscr, TRUE);
+	keypad(stdscr, TRUE);
+	nodelay(stdscr, TRUE);
+	scrollok(stdscr, TRUE);*/
+
+	Display * dpy = XOpenDisplay(NIL);
+	assert(dpy);
+
+	int blackColor = BlackPixel(dpy, DefaultScreen(dpy));
+	int whiteColor = WhitePixel(dpy, DefaultScreen(dpy));
+
+
+	int ch;
+	while (TRUE){
+		if ((ch = getch()) == ERR){
+			// User hasn't hit a key yet
+		}else{
+			// User has hit a key
+			waddch(stdscr, ch | A_UNDERLINE | COLOR_PAIR(3));
+			wrefresh(stdscr);
+		}
+	}
+
+
+	endwin();
 }
